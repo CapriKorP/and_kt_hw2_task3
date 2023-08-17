bcf81a0a677912a3bbbf8f17b646368182f6f24f
pavel <gitkornienkov@gmail.com>
2023-08-17 12:11:55 +0300

    Fix #1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,25 @@
 fun main() {
-    var price = 11_000
-    var regularClient = true
-    var totalDiscount = 0
+    var price = 10_001
+    var regularClient = false
+    var totalPrice = 0
+    var regularDiscount = 0
     val consumerDiscount = 5
     val consumerDiscount100 = 100
     val regularClientDiscount = 1
 
     if (price >= 0 && price <= 1_000) {
-        totalDiscount = price
+        totalPrice = price                                      //Нет скидки
     } else if (price >= 1_001 && price <= 10_000) {
-        totalDiscount = price - consumerDiscount100
+        totalPrice = price - consumerDiscount100                //Скидка 100 руб
     } else if (price >= 10_001) {
-        totalDiscount = price - (price * consumerDiscount / 100)
+        totalPrice = price - (price * consumerDiscount / 100)   //Скидка 5%
     }
 
     if (regularClient) {
-        totalDiscount = totalDiscount - (totalDiscount * regularClientDiscount / 100)
+        regularDiscount = price * regularClientDiscount / 100   //Расчет скидки постоянного клиента
     }
-    println("Сумма Вашей покупки: $price руб. \nCкидка: ${price - totalDiscount} руб. \nСумма покупки со скидкой: $totalDiscount руб.")
+
+    totalPrice -= regularDiscount    //Суммирование скидки постоянного клиента с другими скидками(если есть))
+
+    println("Сумма Вашей покупки: $price руб. \nCкидка: ${price - totalPrice} руб. \nСумма покупки со скидкой: $totalPrice руб.")
 }
