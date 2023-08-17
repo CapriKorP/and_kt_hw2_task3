88266fac4b7f5c96262425d7bb18eafb792e16e2
pavel <gitkornienkov@gmail.com>
2023-08-17 13:05:44 +0300

    Revert "Fix #1"
    This reverts commit bcf81a0a677912a3bbbf8f17b646368182f6f24f.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,21 @@
 fun main() {
-    var price = 10_001
-    var regularClient = false
-    var totalPrice = 0
-    var regularDiscount = 0
+    var price = 11_000
+    var regularClient = true
+    var totalDiscount = 0
     val consumerDiscount = 5
     val consumerDiscount100 = 100
     val regularClientDiscount = 1
 
     if (price >= 0 && price <= 1_000) {
-        totalPrice = price                                      //Нет скидки
+        totalDiscount = price
     } else if (price >= 1_001 && price <= 10_000) {
-        totalPrice = price - consumerDiscount100                //Скидка 100 руб
+        totalDiscount = price - consumerDiscount100
     } else if (price >= 10_001) {
-        totalPrice = price - (price * consumerDiscount / 100)   //Скидка 5%
+        totalDiscount = price - (price * consumerDiscount / 100)
     }
 
     if (regularClient) {
-        regularDiscount = price * regularClientDiscount / 100   //Расчет скидки постоянного клиента
+        totalDiscount = totalDiscount - (totalDiscount * regularClientDiscount / 100)
     }
-
-    totalPrice -= regularDiscount    //Суммирование скидки постоянного клиента с другими скидками(если есть))
-
-    println("Сумма Вашей покупки: $price руб. \nCкидка: ${price - totalPrice} руб. \nСумма покупки со скидкой: $totalPrice руб.")
+    println("Сумма Вашей покупки: $price руб. \nCкидка: ${price - totalDiscount} руб. \nСумма покупки со скидкой: $totalDiscount руб.")
 }
